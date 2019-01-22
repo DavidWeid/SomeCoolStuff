9968fe38010f6259397ff98a053e2ead5bd2b573
David Weid <david.weid.2@gmail.com>
2019-01-22 17:11:05 -0500

    updated boat to spawn where clicked

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ var width = boatImg.get(0).width;
 var screenWidth = $(window).width();
 var duration = 5000;
 
+var cursorX;
+var cursorY;
+
+function getMouseLocation(e) {
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+    console.log(cursorX);
+    console.log(cursorY);
+}
+
 function animateBoat() {
 
-    boatImg.css("left", -width).animate( {
+    boatImg.css("left", cursorX);
+    boatImg.css("top", cursorY);
+
+    boatImg.css("left", cursorX).animate( {
         "left": screenWidth
     }, duration, removeBoat);
 
@@ -15,4 +28,17 @@ function removeBoat() {
     $("#boat").css("display", "none");
 }
 
-animateBoat();
+function returnBoat() {
+    $("#boat").css("visibility", "visible");
+    $("#boat").css("display", "unset");
+}
+
+$(document).on("click", function(e) {
+
+    returnBoat();
+    getMouseLocation(e);
+    animateBoat();
+
+})
+
+// animateBoat();
